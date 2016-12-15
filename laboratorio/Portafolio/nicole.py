a98c777f81029a951f887c54bd197b8d7e635c80
nicole <nicole192@live.com>
2016-12-14 20:28:48 -0500

    fiebre

---
+++ laboratorio/Portafolio/nicole.py
@@ -0,0 +1,22 @@
+cen = float(input("introducir la cantidad de moneda:"))
+
+eur = 0.8965
+yen = 101.5744
+bp = 0.7702
+mxn = 19.7843
+
+dolar = (cen/100)
+
+a = dolar*eur
+b = dolar*yen
+c = dolar*bp
+d = dolar*mxn
+
+print ("convertir a eur:"+str(a))
+print ("convertir a yen:"+str(b))
+print ("convertir a bp:"+str(c))
+print ("convertir a mxn:"+str(d))
+
+
+
+
